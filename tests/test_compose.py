88b3cc3b1c981b71f58ef7a07f936aa1a0ed7978
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2018-01-23 01:04:21 -0400

    Adds more compose tests

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -36,3 +36,31 @@ class TestCompose:
         expected = Delta().delete(1).retain(1, {'bold': True})
         delta = a.compose(b)
         assert delta == expected, [delta.ops, expected.ops]
+
+    def test_delete_delete(self):
+        a = Delta().delete(1)
+        b = Delta().delete(1)
+        expected = Delta().delete(2)
+        delta = a.compose(b)
+        assert delta == expected, [delta.ops, expected.ops]
+
+    def test_retain_insert(self):
+        a = Delta().retain(1, {'bold': True})
+        b = Delta().insert('B')
+        expected = Delta().insert('B').retain(1, {'bold': True})
+        delta = a.compose(b)
+        assert delta == expected, [delta.ops, expected.ops]
+
+    def test_retain_retain(self):
+        a = Delta().retain(1, {'bold': True})
+        b = Delta().retain(1, {'color': 'blue'})
+        expected = Delta().retain(1, {'bold': True, 'color': 'blue'})
+        delta = a.compose(b)
+        assert delta == expected, [delta.ops, expected.ops]
+
+    def test_retain_delete(self):
+        a = Delta().retain(1, {'bold': True})
+        b = Delta().delete(1)
+        expected = Delta().delete(1)
+        delta = a.compose(b)
+        assert delta == expected, [delta.ops, expected.ops]
